ded5b041bd26bc0e4c483349821f0748da7404a4
Sarah Dawson <knittingarch@gmail.com>
2016-04-23 09:23:51 -0400

    base.py changes

--- teamdetails/config/settings/base.py
+++ teamdetails/config/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l0x=0rrem)3!f*1x53=i9_q=3#t7_ox^@q$&j8dkh6ucwff&#w'
+SECRET_KEY = 'it-is-a-secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
