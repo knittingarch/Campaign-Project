4fa0bc8fa369d7848f271592fc99124b092a93b2
Sarah Dawson <knittingarch@gmail.com>
2016-04-25 00:48:00 -0400

    updated static config

--- campaign/urls.py
+++ campaign/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, patterns, url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^', include('poll.urls')),
     url(r'^poll/', include('poll.urls')),
     url(r'^admin/', admin.site.urls),
-]
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
