28fbbf7ca026c434eb8074d8dd52f8359f0bddd0
Sarah Dawson <knittingarch@gmail.com>
2016-04-24 23:57:21 -0400

    settings file sneaky

--- campaign/wsgi.py
+++ campaign/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "campaign.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "campaign.campaign.settings")
 
 application = get_wsgi_application()
