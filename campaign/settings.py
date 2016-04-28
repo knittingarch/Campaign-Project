2f7f5ca5b11f49d61f42abcf6b01799dfbb9f5a9
Sarah Dawson <knittingarch@gmail.com>
2016-04-28 17:17:14 -0400

    fixed degub status for heroku

--- campaign/settings.py
+++ campaign/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'bob'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_STATUS') or 'True'
+DEBUG = os.environ.get('DEBUG_STATUS') or True
 
 ALLOWED_HOSTS = ['*']
 
